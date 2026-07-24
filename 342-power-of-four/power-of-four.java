c76749de6a1ec6e4ec74a66c2b610f5b38f3e14a
Daksh Mehra <dakshmehra817@gmail.com>
2026-07-24 16:47:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (86.27%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0){
+            return false;
+        }
+        else if(n==1){
+            return true;
+        }
+        else{
+            while(n%4==0){
+                n = n/4;
+            }
+            if (n==1){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
